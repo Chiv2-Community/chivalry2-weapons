486a48150f53945d3c5a8eeb406c3ec3d07b8e05
Jacob Barber <jacob.barber@disney.com>
2024-10-11 19:47:32 -0500

    Undo formatting changes to unrelated script

--- scripts/ingest-abilities-override.py
+++ scripts/ingest-abilities-override.py
@@ -1,19 +1,15 @@
-#!/usr/bin/env python3
-
-import argparse
 import json
-import os
 import re
 import sys
+import os
 from collections.abc import Mapping
+import argparse
 
-from common import VALID_ATTACKS, write_dicts_to_csv
+from common import write_dicts_to_csv, VALID_ATTACKS
 from derived_stats import make_average_attack, make_stamina_damage
-
 
 def seconds_to_millis(n):
     return n * 1000 if n != -1 else -1
-
 
 STAT_TRANSFORMS = {
     "windup": seconds_to_millis,
@@ -26,23 +22,16 @@ STAT_TRANSFORMS = {
 }
 
 # When the stats on the left are undefined (-1 or 0), they should fall back to the stat on the right.
-STAT_FALLBACKS = {"riposte": "windup", "thwack": "release"}
-
+STAT_FALLBACKS = {
+    "riposte": "windup",
+    "thwack": "release"
+}
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-i", "--input_json", required=True, help="Path to the input JSON file"
-    )
-    parser.add_argument(
-        "-o", "--output_dir", required=True, help="Path to the output directory"
-    )
-    parser.add_argument(
-        "-c",
-        "--changelog_location",
-        required=True,
-        help="Path to output the changelog json",
-    )
+    parser.add_argument("-i", "--input_json", required=True, help="Path to the input JSON file")
+    parser.add_argument("-o", "--output_dir", required=True, help="Path to the output directory")
+    parser.add_argument("-c", "--changelog_location", required=True, help="Path to output the changelog json")
     args = parser.parse_args()
 
     base_defaults = {}
@@ -53,50 +42,40 @@ def main():
     data = fetch_data(args.input_json)[0]["Rows"]
 
     for name, item in data.items():
-        process_item(
-            name, item, base_defaults, attack_defaults, weapon_defaults, weapons
-        )
+        process_item(name, item, base_defaults, attack_defaults, weapon_defaults, weapons)
 
     apply_defaults(weapons, attack_defaults)
 
     write_to_file(list(weapons.values()), args.output_dir, args.changelog_location)
 
-
 def lowercase_first_char(in_str):
     return in_str[0].lower() + in_str[1:]
 
-
 def fetch_data(path):
     with open(path) as user_file:
-        return json.load(user_file)
+      return json.load(user_file)
 
-
-def clean_item(item):  # , VALID_STATS):
-    return {lowercase_first_char(key): item[key] for key in item.keys()}  # VALID_STATS}
-
+def clean_item(item):#, VALID_STATS):
+    return {lowercase_first_char(key): item[key] for key in item.keys()}#VALID_STATS}
 
 def process_item(name, item, base_defaults, attack_defaults, weapon_defaults, weapons):
-    name_parts = name.split(".")
-    item = clean_item(item)  # , VALID_STATS)
+    name_parts = name.split('.')
+    item = clean_item(item)#, VALID_STATS)
     attack_type = lowercase_first_char(name_parts[1]) if len(name_parts) > 1 else None
 
-    if attack_type and attack_type not in VALID_ATTACKS:
+    if attack_type and attack_type not in VALID_ATTACKS: 
         return
 
-    if name_parts[0] == "Default":
-        process_default_item(
-            name_parts, attack_type, item, base_defaults, attack_defaults
-        )
+    if name_parts[0] == 'Default':
+        process_default_item(name_parts, attack_type, item, base_defaults, attack_defaults)
     else:
         process_weapon_item(name_parts, attack_type, item, weapon_defaults, weapons)
-
 
 def process_default_item(name_parts, attack_type, item, base_defaults, attack_defaults):
     if len(name_parts) == 1:
         base_defaults = item
     else:
         attack_defaults = process_attack(attack_type, item, attack_defaults)
-
 
 def process_weapon_item(name_parts, attack_type, item, weapon_defaults, weapons):
     if len(name_parts) == 1:
@@ -105,10 +84,7 @@ def process_weapon_item(name_parts, attack_type, item, weapon_defaults, weapons)
         weapon_name = name_parts[0].replace("Weapon_", "")
         if weapon_name not in weapons:
             weapons[weapon_name] = {"name": weapon_name, "attacks": {}}
-        weapons[weapon_name]["attacks"] = process_attack(
-            attack_type, item, weapons[weapon_name]["attacks"]
-        )
-
+        weapons[weapon_name]["attacks"] = process_attack(attack_type, item, weapons[weapon_name]["attacks"])
 
 def process_attack(attack_type, item, attacks):
     if "Heavy" in attack_type:
@@ -126,16 +102,14 @@ def process_attack(attack_type, item, attacks):
         attacks[attack_type] = apply_stat_transforms(item)
     return attacks
 
-
 def derive_stats(weapon):
     if "slash" not in weapon["attacks"]:
-        return
+        return 
 
     if "id" in weapon:
         make_stamina_damage(weapon)
 
     make_average_attack(weapon)
-
 
 def apply_defaults(weapons, attack_defaults):
     for weapon, weapon_data in weapons.items():
@@ -150,7 +124,6 @@ def apply_defaults(weapons, attack_defaults):
                     if attack_data[key] == -1:
                         if key in attack_defaults.get(attack, {}):
                             attack_data[key] = attack_defaults[attack][key]
-
 
 def write_to_file(data, foldername, changelog_location):
     try:
@@ -167,7 +140,7 @@ def write_to_file(data, foldername, changelog_location):
             if exists:
                 existing_data = fetch_data(path)
 
-            with open(path, "w") as outfile:
+            with open(path, 'w') as outfile:
                 (changes, merged) = deep_merge(weapon["name"], existing_data, weapon)
                 derive_stats(merged)
                 if len(changes) > 0:
@@ -176,36 +149,22 @@ def write_to_file(data, foldername, changelog_location):
                 merged_weapons.append(merged)
                 json.dump(merged, outfile, indent=2)
 
-        for name, changes in changelog.items():
+        for (name, changes) in changelog.items():
             for change in changes:
-                full_path = name + "." + ".".join(change["path"])
-
-                print(
-                    full_path + ": " + str(change["old"]) + " -> " + str(change["new"])
-                )
-
-        # write_dicts_to_csv(data, foldername + "/data.csv")
+                full_path = name + "." + '.'.join(change['path'])
+		
+                print(full_path + ": " + str(change['old']) + " -> " + str(change['new']))
+        
+        #write_dicts_to_csv(data, foldername + "/data.csv")
 
         changelog_text = ""
-        for name, changes in changelog.items():
+        for (name, changes) in changelog.items():
             changelog_text += name + ":\n"
             for change in changes:
-                new_string = (
-                    str(change["new"])
-                    if not isinstance(change["new"], dict)
-                    else json.dumps(change["new"], indent=12)
-                )
-                changelog_text += (
-                    "\t"
-                    + ".".join(change["path"])
-                    + ": "
-                    + str(change["old"])
-                    + " -> "
-                    + new_string
-                    + "\n"
-                )
+                new_string = str(change['new']) if not isinstance(change['new'], dict) else json.dumps(change['new'], indent=12)
+                changelog_text += "\t" + '.'.join(change['path']) + ": " + str(change['old']) + " -> " + new_string + "\n"
 
-        with open(changelog_location, "w") as changelog_file:
+        with open(changelog_location, 'w') as changelog_file: 
             changelog_file.write(changelog_text)
 
     except IOError as e:
@@ -214,18 +173,16 @@ def write_to_file(data, foldername, changelog_location):
 
 
 def pascal_to_camel(s):
-    return re.sub(r"(?<!^)(?=[A-Z])", "_", s).lower()
-
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', s).lower()
 
 def pascal_to_space(s):
-    return re.sub(r"(?<!^)(?=[A-Z])", " ", s).replace("  ", " ")
-
+    return re.sub(r'(?<!^)(?=[A-Z])', ' ', s).replace("  ", " ")
 
 def apply_stat_transforms(data):
     for key, value in data.items():
         if key in STAT_TRANSFORMS:
             data[key] = STAT_TRANSFORMS[key](value)
-
+    
     # after applying transformations, apply fallbacks
     for key, value in data.items():
         if value in [-1, 0]:
@@ -236,36 +193,31 @@ def apply_stat_transforms(data):
 
     return data
 
-
 def deep_merge(name, dict1, dict2, path=None):
     "Deeply merge two dictionaries and print out the differences."
 
     changes = []
 
-    if path is None:
+    if path is None: 
         path = []
 
     for key in dict2:
         value = dict2[key]
         if key in dict1:
             if isinstance(dict1[key], Mapping) and isinstance(value, Mapping):
-                (new_changes, _) = deep_merge(
-                    name, dict1[key], value, path + [str(key)]
-                )
+                (new_changes, _) = deep_merge(name, dict1[key], value, path + [str(key)])
                 changes += new_changes
-            else:
+            else: 
                 if dict1[key] == value:
-                    pass  # same leaf value
+                    pass # same leaf value
                 else:
-                    changes.append(
-                        {"path": path + [key], "old": dict1[key], "new": value}
-                    )
+                    changes.append({'path': path + [key], 'old': dict1[key], 'new': value})
                     dict1[key] = value
         else:
-            changes.append({"path": path + [key], "old": None, "new": value})
+            changes.append({'path': path + [key], 'old': None, 'new': value})
             dict1[key] = value
     return (changes, dict1)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
